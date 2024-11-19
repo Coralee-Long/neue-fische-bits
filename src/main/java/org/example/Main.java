172e834724d2f32e65bb2a5fda059700de9afe0f
Coralee Long <coralee@coralees-mbp.localdomain>
2024-11-19 14:35:06 +0100

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
+
+
+/* git setup refresher
+
+    git init
+    git remote add origin git@github.com:Coralee-Long/neue-fische-bits.git
+    git remote -v (verify the remote)
+
+*/
